f9ec2edd890776c69de3ba19244b14a1cff80e90
nylespham <nylespham@gmail.com>
2026-02-14 15:38:46 +0700

    VSE-15 Onboard new repo for Inventium

--- local_ecr.tf
+++ local_ecr.tf
@@ -1,11 +1,12 @@
 locals {
   apps_ecr = {
-    inventory-service    = { "name" = "inventory-service" },
-    sale-service         = { "name" = "sale-service" },
-    pos-service          = { "name" = "pos-service" },
-    warehouse-service    = { "name" = "warehouse-service" },
-    frontend-service     = { "name" = "frontend-service" },
-    notification-service = { "name" = "notification-service" },
+    inventory-service             = { "name" = "inventory-service" },
+    inventory-transaction-service = { "name" = "inventory-transaction-service" },
+    sale-service                  = { "name" = "sale-service" },
+    pos-service                   = { "name" = "pos-service" },
+    warehouse-service             = { "name" = "warehouse-service" },
+    frontend-service              = { "name" = "frontend-service" },
+    notification-service          = { "name" = "notification-service" },
   }
   tools_ecr = {
     clerk = { "name" = "clerk" },
--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ module "inventium_ecr" {
   image_tag_mutability = "IMMUTABLE"
 }
 
+module "inventium_ecr_cache" {
+  source               = "./module/ecr"
+  for_each             = local.apps_ecr
+  name                 = "helper/inventium/${each.value["name"]}"
+  image_tag_mutability = "IMMUTABLE"
+}
+
 module "varsentinel_ecr" {
   source               = "./module/ecr"
   for_each             = local.tools_ecr
